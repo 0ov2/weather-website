9e7a53ca4264859bc9e6d79612b5f1a1fda35dc8
Liam Dropinski-Evans <dropilw6@mc-531276.immediate.media>
2019-08-12 16:07:38 +0100

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request')
+
+const forecast = (lat, long, callback) => {
+    const url = 'https://api.darksky.net/forecast/7cd0f26f2ff54d180e1b6311d4aed489/'+ lat + ',' + long +''
+    request({url, json: true}, (error, { body }) => {
+        if (error) {
+            callback('unable to connect to weather services', undefined)
+        } else if (body.error) {
+            callback('unable to find location. please try again.', undefined)
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' The current temp is ' + body.currently.temperature + '. there is a ' + body.currently.precipProbability + '% chance of rain')
+        }
+    })
+}
+
+module.exports = forecast
